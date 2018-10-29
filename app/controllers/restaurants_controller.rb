a085c662a5aac0773a94eea4389b3e35dd4b2cd2
wang0805 <wh_wang@live.com>
2018-10-29 15:39:39 +0800

    add restro views n cloudinary

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -6,19 +6,29 @@ class RestaurantsController < ApplicationController
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def new
+    # if im a userrest then i can add
+    @restaurant = Restaurant.new
   end
 
   def create
     # render plain: params[:restaurant].inspect
-    @restaurant = Restaurant.new(post_params) 
+    @restaurant = Restaurant.new(restro_params) 
+    if @restaurant.image_url
+      uploaded_file = params[:restaurant][:image_url].path
+      cloudnary_file = Cloudinary::Uploader.upload(uploaded_file)
+      @restaurant.image_url = cloudnary_file['public_id']
+    # else
+    #   @restaurant.photo_url = $result.data.image_url
+    end
 
     if @restaurant.save
       redirect_to @restaurant
     else
-      render 'new'
+      
     end
   end
 
@@ -46,5 +56,5 @@ class RestaurantsController < ApplicationController
 
   private
   def restro_params
-    parmas.require(:restaurant).permit(:name, :category, :location, :lat, :long)
+    params.require(:restaurant).permit(:name, :category, :location, :lat, :long, :image_url, :image2_url, :image3_url)
 end
