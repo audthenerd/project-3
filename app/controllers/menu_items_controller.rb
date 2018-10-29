cf50ee7fc07d15b22ab987cb842a5dfc63e14067
claraonmars <claraonmars@hotmail.com>
2018-10-29 16:48:53 +0800

    Complete RESTful routes for menu_items

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -4,15 +4,24 @@ class MenuItemsController < ApplicationController
   def index
     @restaurant = Restaurant.find(params[:restaurant_id])
     @menuitems = MenuItem.where(restaurant_id: params[:restaurant_id])
+
   end
 
   def create
+    @menuitem = MenuItem.new(menu_item_params)
+    @menuitem.save
+
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+
+    redirect_to restaurant_menu_items_path(@restaurant)
   end
 
   def new
   end
 
   def edit
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @menuitem = MenuItem.find(params[:id])
   end
 
@@ -20,9 +29,22 @@ class MenuItemsController < ApplicationController
   end
 
   def update
+    @menuitem = MenuItem.find(params[:id])
+    @menuitem.update(menu_item_params)
+
+    @restaurant = Restaurant.find(params[:menu_item][:restaurant_id])
+
+    redirect_to restaurant_menu_items_path(@restaurant)
   end
 
   def destroy
+    @menuitem = MenuItem.find(params[:id])
+    @menuitem.delete
+
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+
+    redirect_to restaurant_menu_items_path(@restaurant)
   end
 
   private
