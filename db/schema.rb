b8b0fbe681288a4084dbdcf1af956d12c75cf0f9
wang0805 <wh_wang@live.com>
2018-10-29 10:38:35 +0800

    change category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_27_195542) do
+ActiveRecord::Schema.define(version: 2018_10_29_023630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,10 +63,10 @@ ActiveRecord::Schema.define(version: 2018_10_27_195542) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.text "catogery"
     t.text "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "category"
   end
 
   add_foreign_key "menu_items", "restaurants"
